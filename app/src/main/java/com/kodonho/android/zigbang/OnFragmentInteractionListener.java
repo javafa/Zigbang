4f8e71cbb48bebbddeace9011aae4797553a2841
javafa <nigne2016@gmail.com>
2016-11-08 17:35:12 +0900

    최초등록

---
+++ app/src/main/java/com/kodonho/android/zigbang/OnFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package com.kodonho.android.zigbang;
+
+import android.net.Uri;
+
+/**
+ * Created by fastcampus on 2016-11-08.
+ */
+
+public interface OnFragmentInteractionListener {
+    void onFragmentInteraction(Uri uri);
+}
